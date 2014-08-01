f59542251d4cb1f948fd3591f2541e3c92786044
Joe Mordetsky <jmordetsky@zynga.com>
2014-08-01 16:57:53 -0400

    First commit from yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularDemoApp
+ * @description
+ * # angularDemoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularDemoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
